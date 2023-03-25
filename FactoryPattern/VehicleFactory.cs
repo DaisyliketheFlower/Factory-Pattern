b6f0fe75c83901e900e4312ee23843d375ae791c
daisy.omoikirodah <thesmexxiichic@gmail.com>
2023-03-25 18:14:20 -0400

    done

---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace FactoryPattern
+{
+	public class VehicleFactory
+	{
+		public static IVehicle GetVehicle(int numberOfTires)
+		{
+			if (numberOfTires == 2)
+			{
+				return new Motorcycle();
+			}
+			else if (numberOfTires == 4)
+			{
+				return new Car();
+			}
+			else
+			{
+				return new Car();
+			}
+			//jeremy said If else statement was fine
+		}
+
+	}
+}
